7d5bdd68e37b176f3f13f55a13b59afc7b889a78
Inna Antypova <nightallatiel@gmail.com>
2023-11-25 16:50:30 +0300

    fix

--- src/hh_api.py
+++ src/hh_api.py
@@ -32,48 +32,74 @@ class HeadHunterAPI(ABCApi):
         data = requests.get(url=employer_url, headers=HeadHunterAPI.headers, timeout=10).json()
 
         employer_info = []
-        name = data['name']
-        description = data['description'].replace('<p>', '').replace('<strong>', '').replace('</p>', ''). \
-            replace('</strong>', '').replace('&ndash;', '').replace(';&ndash;', '').replace('&nbsp;', '')
-        employer_url = data['site_url']
-        employer_info.append({
-            'name': name,
-            'description': description,
-            'employer_url': employer_url})
-        return employer_info
+        try:
+            name = data['name']
+            description = data['description'].replace('<p>', '').replace('<strong>', '').replace('</p>', ''). \
+                replace('</strong>', '').replace('&ndash;', '').replace(';&ndash;', '').replace('&nbsp;', '').\
+                replace('<br />', '').replace('</li>', '').replace('<li>', '').replace('<u>', '').replace('</u>', '').\
+                replace('&quot;', '').replace('</ul>', '').replace('<ul>', '')
+            employer_url = data['site_url']
+            employer_info.append({
+                'name': name,
+                'description': description,
+                'employer_url': employer_url})
+            if not employer_info:
+                raise ValueError
+
+            return employer_info
+
+        except ValueError:
+            raise ValueError('Ошибка данных.')
 
     def get_vacancies_from_api(self, employer_id: str) -> list[dict[str, Any]]:
         """ Метод для получения вакансий конкретного работодателя по его ID. """
         vacancies_url = f"https://api.hh.ru/vacancies?employer_id={employer_id}"
-        data = requests.get(url=vacancies_url, headers=HeadHunterAPI.headers, timeout=10).json()['items']
+        param = {
+            'page': 0,
+            'per_page': 100,
+        }
+        data = requests.get(url=vacancies_url, headers=HeadHunterAPI.headers, params=param, timeout=10).json()['items']
 
         vacancies_info = []
-        for vacancy in data:
-            if vacancy['salary']:
-                salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] else 0
-                salary_to = vacancy['salary']['to'] if vacancy['salary']['to'] else 0
-            else:
-                salary_from = 0
-                salary_to = 0
+        try:
+            for vacancy in data:
+                if vacancy['salary']:
+                    salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] else 0
+                    salary_to = vacancy['salary']['to'] if vacancy['salary']['to'] else 0
+                else:
+                    salary_from = 0
+                    salary_to = 0
 
-            if vacancy['address']:
-                address = vacancy['address']['raw'] if vacancy['address']['raw'] else ''
-            else:
-                address = ''
+                if vacancy['address']:
+                    city = vacancy['address']['city']
+                    street = vacancy['address']['street'] if vacancy['address']['street'] else ''
+                    building = vacancy['address']['building'] if vacancy['address']['building'] else ''
+                else:
+                    city = ''
+                    street = ''
+                    building = ''
 
-            published = datetime.fromisoformat(vacancy['published_at']).strftime('%d-%m-%Y')
-            responsibility = vacancy['snippet']['responsibility'] if vacancy['snippet']['responsibility'] else ''
-            requirement = vacancy['snippet']['requirement'] if vacancy['snippet']['requirement'] else ''
+                address = f'{street} {building}'
+                published = datetime.fromisoformat(vacancy['published_at']).strftime('%d-%m-%Y')
+                responsibility = vacancy['snippet']['responsibility'] if vacancy['snippet']['responsibility'] else ''
+                requirement = vacancy['snippet']['requirement'] if vacancy['snippet']['requirement'] else ''
 
-            vacancies_info.append({
-                'vacancy_name': vacancy['name'],
-                'address': address,
-                'salary_from': salary_from,
-                'salary_to': salary_to,
-                'published': published,
-                'vacancy_url': vacancy['alternate_url'],
-                'area': vacancy['area']['name'],
-                'employment': vacancy['employment']['name'],
-                'requirement': requirement,
-                'responsibility': responsibility})
-        return vacancies_info
+                vacancies_info.append({
+                    'vacancy_name': vacancy['name'],
+                    'address': address,
+                    'salary_from': salary_from,
+                    'salary_to': salary_to,
+                    'published': published,
+                    'vacancy_url': vacancy['alternate_url'],
+                    'city': city,
+                    'employment': vacancy['employment']['name'],
+                    'requirement': requirement,
+                    'responsibility': responsibility})
+
+            if not vacancies_info:
+                raise ValueError
+
+            return vacancies_info
+
+        except ValueError:
+            raise ValueError('Ошибка данных.')
